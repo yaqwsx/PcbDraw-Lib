31d704791ed4acaa3124fdb3c273883ea05dc9b1
Jan Mrázek <email@honzamrazek.cz>
2021-05-30 23:06:29 +0200

    Add option to generate templates for the whole board at once

--- scripts/create_template.py
+++ scripts/create_template.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python3
 
 from pcbdraw import pcbdraw
-import pcbnew
+from pcbnewTransition import pcbnew
 import click
 import os
+from pathlib import Path
 
 from lxml import etree
-from pcbdraw.pcbdraw import ki2mm, ki2dmil, mm2ki
+from pcbdraw.pcbdraw import ki2mm, ki2svg, mm2ki
 
 def loadFootprint(footprintPath):
     lib, foot = os.path.split(footprintPath)
@@ -24,6 +25,26 @@ def buildFootprintBoard(footprintPath):
     footprint.SetPosition(pcbnew.wxPoint(0, 0))
     footprint.Reference().SetVisible(False)
     footprint.Value().SetVisible(False)
+    board.Add(footprint)
+    return board
+
+def buildFootprintBoardF(footprint):
+    """
+    Given a footprint file, build a single board with the component on
+    0,0 in the default orientation.
+    """
+    # print(footprint)
+    try:
+        newFootprint = footprint.Duplicate()
+    except TypeError: # Footprint has overridden the method, cannot be called directly
+        print("Here")
+        newFootprint = pcbnew.Cast_to_BOARD_ITEM(footprint).Duplicate()
+    board = pcbnew.BOARD()
+    if footprint.GetLayerName() == pcbnew.B_Cu:
+        footprint.Flip(pcbnew.wxPoint(0, 0), True)
+    footprint.SetPosition(pcbnew.wxPoint(0, 0))
+    # footprint.Reference().SetVisible(False)
+    # footprint.Value().SetVisible(False)
     board.Add(footprint)
     return board
 
@@ -47,7 +68,7 @@ def plotTopLayers(board):
             ("cmt", [pcbnew.Cmts_User], process),
             ("edge", [pcbnew.Edge_Cuts], process),
             ("silk", [pcbnew.F_SilkS], process)]
-    elements = pcbdraw.get_layers(board, colors, plotPlan)
+    elements = pcbdraw.get_layers(board, colors, {}, plotPlan)
     elements.attrib["id"] = "KiCAD footprint top"
     return elements
 
@@ -71,7 +92,7 @@ def plotBottomLayers(board):
             ("cmt", [pcbnew.Cmts_User], process),
             ("edge", [pcbnew.Edge_Cuts], process),
             ("silk", [pcbnew.B_SilkS], process)]
-    elements = pcbdraw.get_layers(board, colors, plotPlan)
+    elements = pcbdraw.get_layers(board, colors, {}, plotPlan)
     elements.attrib["id"] = "KiCAD footprint bottom"
     return elements
 
@@ -82,15 +103,15 @@ def addOrigin(document):
     origin = etree.Element("rect")
     origin.attrib["id"] = "origin"
     origin.attrib["fill"] = "red"
-    origin.attrib["width"] = str(ki2dmil(mm2ki(1)))
-    origin.attrib["height"] = str(ki2dmil(mm2ki(1)))
+    origin.attrib["width"] = str(ki2svg(mm2ki(1)))
+    origin.attrib["height"] = str(ki2svg(mm2ki(1)))
     origin.attrib["x"] = "0"
     origin.attrib["y"] = "0"
 
     document.getroot().append(origin)
 
 
-@click.command()
+@click.command("footprint")
 @click.argument("footprint", type=click.Path(exists=True, file_okay=True, dir_okay=False))
 @click.argument("output", type=click.File(mode="wb"))
 @click.option("--front", "front", flag_value=True,
@@ -99,7 +120,7 @@ def addOrigin(document):
     help="Render back size of the footprint")
 @click.option("--shrink", type=bool, default=False,
     help="Shrink template to size")
-def run(footprint, output, front, shrink):
+def run_footprint(footprint, output, front, shrink):
     """
     Create a template for footprint based on the KiCAD footprint file.
     """
@@ -108,7 +129,7 @@ def run(footprint, output, front, shrink):
     document = pcbdraw.empty_svg(
             width=f"{ki2mm(bb.GetWidth())}mm",
             height=f"{ki2mm(bb.GetHeight())}mm",
-            viewBox=f"{ki2dmil(bb.GetX())} {ki2dmil(bb.GetY())} {ki2dmil(bb.GetWidth())} {ki2dmil(bb.GetHeight())}")
+            viewBox=f"{ki2svg(bb.GetX())} {ki2svg(bb.GetY())} {ki2svg(bb.GetWidth())} {ki2svg(bb.GetHeight())}")
     if front:
         document.getroot().append(plotTopLayers(board))
     else:
@@ -124,5 +145,66 @@ def run(footprint, output, front, shrink):
         output.close()
         pcbdraw.shrink_svg(path, 0)
 
+@click.command("board")
+@click.argument("board", type=click.Path(exists=True, file_okay=True, dir_okay=False))
+@click.argument("output", type=click.Path(exists=False, file_okay=False, dir_okay=True))
+@click.option("--shrink", type=bool, default=False,
+    help="Shrink template to size")
+def run_board(board, output, shrink):
+    """
+    Create a whole library of templates for footprints on given board
+    """
+    board = pcbnew.LoadBoard(board)
+    outdir = Path(output)
+    outdir.mkdir(parents=True, exist_ok=True)
+    footprints = {
+        (str(f.GetFPID().GetLibNickname()), str(f.GetFPID().GetLibItemName())): f
+        for f in board.GetFootprints()}
+    for (lib, name), f in footprints.items():
+        print(f"Plotting: {lib}:{name}")
+        fBoard = buildFootprintBoardF(f)
+        bb = fBoard.ComputeBoundingBox()
+        outFile = outdir / lib / (name + ".svg")
+        document = pcbdraw.empty_svg(
+            width=f"{ki2mm(bb.GetWidth())}mm",
+            height=f"{ki2mm(bb.GetHeight())}mm",
+            viewBox=f"{ki2svg(bb.GetX())} {ki2svg(bb.GetY())} {ki2svg(bb.GetWidth())} {ki2svg(bb.GetHeight())}")
+        g = plotBottomLayers(fBoard)
+        document.getroot().append(g)
+        addOrigin(document)
+        with open(outFile, "wb") as f:
+            document.write(f)
+        if shrink:
+            path = output.name
+            output.close()
+            pcbdraw.shrink_svg(path, 0)
+
+        outFile = outdir / lib / (name + ".back.svg")
+        document = pcbdraw.empty_svg(
+            width=f"{ki2mm(bb.GetWidth())}mm",
+            height=f"{ki2mm(bb.GetHeight())}mm",
+            viewBox=f"{ki2svg(bb.GetX())} {ki2svg(bb.GetY())} {ki2svg(bb.GetWidth())} {ki2svg(bb.GetHeight())}")
+        g = plotBottomLayers(fBoard)
+        g.attrib["transform"] = "scale(-1 1)"
+        document.getroot().append(g)
+        addOrigin(document)
+        with open(outFile, "wb") as f:
+            document.write(f)
+        if shrink:
+            path = output.name
+            output.close()
+            pcbdraw.shrink_svg(path, 0)
+
+
+@click.group()
+def cli():
+    """
+    Create footprint templates
+    """
+    pass
+
+cli.add_command(run_footprint)
+cli.add_command(run_board)
+
 if __name__ ==  "__main__":
-    run()
+    cli()
