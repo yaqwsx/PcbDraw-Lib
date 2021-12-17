4723743ad09a7bac8893aa5650d4b211fc4d43c0
Electro707 <general@electro707.com>
2021-12-16 21:25:52 -0500

    Changed the DIP packaged to make it nicer. Updated the DIP generating script with more comments

--- scripts/generate_dip.py
+++ scripts/generate_dip.py
@@ -16,26 +16,44 @@ if __name__ == "__main__":
 
     args = parser.parse_args()
     
-    for pins in [4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24, 28, 32]:
+    for pins in [6, 8, 10, 12, 14, 16, 18, 20, 22, 24, 28, 32]:
+        # Get the document's items
         document = etree.parse(args.model)
         root = document.getroot()
+        
+        # Delete all inkscape grids
+        p = root.find("{http://sodipodi.sourceforge.net/DTD/sodipodi-0.dtd}namedview")
+        for c in p.findall("{http://www.inkscape.org/namespaces/inkscape}grid"):
+            p.remove(c)
+        
         origin = root.find(".//*[@id='origin']")
         body = root.find(".//*[@id='body_path']")
         first_pin_dot = root.find(".//*[@id='first_pin_dot']")
+        # Remove the origin, body, and first dot pin to add later
+        # This is to place them above the pins
         root.remove(origin)
         root.remove(body)
         root.remove(first_pin_dot)
-        for i in range(pins//2):
-          for p in range(1, 3):
-            pin = root.find(".//*[@id='pin{}']".format(p))
+        # For all middle pins, copy the left and right one in the template and shift it down
+        for i in range(1, (pins//2)-2):
+            for p in range(1, 2+1):
+                pin = root.find(".//*[@id='m-pin{}']".format(p))
+                el = deepcopy(pin)
+                del el.attrib["id"]
+                el.attrib["transform"] = "translate(0 {})".format(i * 2.54)
+                root.append(el)
+        # Move the outside pin
+        for p in range(1, 2+1):
+            pin = root.find(".//*[@id='b-pin{}']".format(p))
             el = deepcopy(pin)
             del el.attrib["id"]
-            el.attrib["transform"] += " translate({} 0)".format(i * 2.54)
+            el.attrib["transform"] = "translate(0 {})".format(((pins//2)-1) * 2.54)
             root.append(el)
-        
-        body.attrib["d"] = body.attrib["d"].replace("FILL_HERE", str((pins//2) * 2.54))
-        
+        # Change the main body's lower dimension
+        body.attrib["d"] = body.attrib["d"].replace("8.3819999", str((pins//2 * 2.54)-(0.02*25.4)))
+        # Add back the body, dot, and origin to the SVG file to lay them on top of the pins
         root.append(body)
         root.append(first_pin_dot)
         root.append(origin)
+        # Export the document
         document.write(args.file_template.format(pins))
