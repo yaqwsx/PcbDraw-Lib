f12b2db71bb350827f1feb793cdb7a24443b7150
Electro707 <general@electro707.com>
2022-11-27 02:28:06 -0500

    Changed SOIC script and base to support multiple width and bug fixes

--- scripts/generate_soic.py
+++ scripts/generate_soic.py
@@ -7,28 +7,30 @@ Models are generated in current working directory.
 import os
 from lxml import etree
 from copy import deepcopy
-import argparse 
+import argparse
 
-soic_height = {
-  8: 4.9,
-  14: 8.7,
-  16: 9.9,
-}
-soic_body_width = 3.9
-lead_width = 1.0
 corner_rad = 0.3
 
-if __name__ == "__main__":    
-    for pins in [8, 14, 16]:
+# TODO: width settings does nothing right now
+soic_settings = [
+  {'pins': 8, 'width': 3.9, 'height': 4.9},
+  {'pins': 8, 'width': 5.3, 'height': 5.3},
+  {'pins': 8, 'width': 5.23, 'height': 5.23},
+  {'pins': 14, 'width': 3.9, 'height': 8.7},
+  {'pins': 16, 'width': 3.9, 'height': 9.9},
+]
+
+if __name__ == "__main__":
+    for soic in soic_settings:
         # Get the document's items
-        document = etree.parse('base/SOIC_W3.9.svg')
+        document = etree.parse('base/SOIC.svg')
         root = document.getroot()
-        
+
         # Delete all inkscape grids
         p = root.find("{http://sodipodi.sourceforge.net/DTD/sodipodi-0.dtd}namedview")
         for c in p.findall("{http://www.inkscape.org/namespaces/inkscape}grid"):
             p.remove(c)
-        
+
         origin = root.find(".//*[@id='origin']")
         body = root.find(".//*[@id='body_path']")
         first_pin_dot = root.find(".//*[@id='first_pin_dot']")
@@ -37,30 +39,29 @@ if __name__ == "__main__":
         root.remove(origin)
         root.remove(body)
         root.remove(first_pin_dot)
-        # Move pins up and down
-        for i in range(1, pins//4):
-            for l_r in ['l', 'r']:
-                for u_p in ['u', 'd']:
-                    pin = root.find(".//*[@id='{}-{}-pin']".format(l_r, u_p))
-                    el = deepcopy(pin)
-                    del el.attrib["id"]
-                    if u_p == 'u':
-                      el.attrib["transform"] = "translate(0 {})".format(-i * 1.27)
-                    else:
-                      el.attrib["transform"] = "translate(0 {})".format(i * 1.27)
-                    root.append(el)
+        sh = soic['height']
+        sw = soic['width']
+        # Copy the pin and use that to make other pins
+        pin_copy = root.find(".//*[@id='pin']")
+        root.remove(pin_copy)
+        for r in [-1, 1]:
+            for c in range(1, soic['pins']//2+1):
+                el = deepcopy(pin_copy)
+                del el.attrib["id"]
+                el.attrib["y"] = "{}".format((soic['pins']/4-c)* 1.27 - 0.2 + 1.27/2)
+                el.attrib["x"] = "{}".format(r*(sw/2 + 0.5) - 0.5)
+                root.append(el)
         # Change the main body's lower dimension
-        sh = soic_height[pins]
-        body.attrib["d"] = "m {},{} h 3.3 c 0,0 0.3,0 0.3,0.3 l 0,{} c 0,0.3 -0.3,0.3 -0.3,0.3 h -3.3 c 0,0 -0.3,0 -0.3,-0.3 l 0,{} c 0,-0.3 0.3,-0.3 0.3,-0.3 z".format(corner_rad-soic_body_width/2, -sh/2, sh-corner_rad*2, -(sh-corner_rad*2))
+        body.attrib["d"] = "m {},{} h {} c 0,0 0.3,0 0.3,0.3 l 0,{} c 0,0.3 -0.3,0.3 -0.3,0.3 h {} c 0,0 -0.3,0 -0.3,-0.3 l 0,{} c 0,-0.3 0.3,-0.3 0.3,-0.3 z".format(corner_rad-sw/2, -sh/2, sw-corner_rad*2, sh-corner_rad*2, -(sw-corner_rad*2), -(sh-corner_rad*2))
         # Move the dot
         first_pin_dot.attrib["cy"] = str(-((sh/2) - 0.8))
-        first_pin_dot.attrib["cx"] = str(-((soic_body_width/2) - 0.8))
+        first_pin_dot.attrib["cx"] = str(-((sw/2) - 0.8))
         # Add back the body, dot, and origin to the SVG file to lay them on top of the pins
         root.append(body)
         root.append(first_pin_dot)
         root.append(origin)
         # Export the document
-        sn = "export/SOIC_{}.svg".format(pins)
+        sn = "export/SOIC-{}_{:.2f}x{:.2f}mm_P1.27mm.svg".format(soic['pins'], sw, sh)
         if not os.path.isdir("export"):
           os.mkdir("export")
         document.write(sn)
